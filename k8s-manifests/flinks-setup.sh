cc613895bac48cf5adee3df809631d7c51873b5c
Nir <nir@pop-os-home.localdomain>
2020-09-12 18:45:42 -0500

    comment added in web-server-daemonset

--- k8s-manifests/flinks-setup.sh
+++ k8s-manifests/flinks-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Deploy nginx ingress controller (values.yaml kind if set to DaemonSet)
-helm install ingress-nginx ./ingress-nginx/ --namespace ingress-controller --dry-run
+helm install ingress-nginx ./ingress-nginx/ --namespace ingress-controller
 
 # Deploy the environment
 kubectl apply -f nginx-web-server-daemonset.yaml -f nginx-web-server-ingress.yaml -f nginx-web-server-service.yaml
