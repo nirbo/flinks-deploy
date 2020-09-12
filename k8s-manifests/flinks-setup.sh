2e16a5d9118ed066959c2271b433d98208a16876
Nir <nir@pop-os-home.localdomain>
2020-09-12 18:11:24 -0500

    Everything working except for SSL

---
+++ k8s-manifests/flinks-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Deploy nginx ingress controller (values.yaml kind if set to DaemonSet)
+helm install ingress-nginx ./ingress-nginx/ --namespace ingress-controller --dry-run
+
+# Deploy the environment
+kubectl apply -f nginx-web-server-daemonset.yaml -f nginx-web-server-ingress.yaml -f nginx-web-server-service.yaml
+
