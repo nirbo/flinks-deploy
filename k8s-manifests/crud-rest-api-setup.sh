00ea2f4cc7632c52c9675206a5dbed336c4b61f1
Nir Ben-Or <nirbo2@gmail.com>
2020-09-13 12:16:12 -0500

    Modified the REST API app to deployment and a single replicas - it now works and is accessible from the internet

--- k8s-manifests/crud-rest-api-setup.sh
+++ k8s-manifests/crud-rest-api-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Deploy the CRUD REST API
-kubectl apply -f crud-rest-api-daemonset.yaml -f crud-rest-api-ingress.yaml -f crud-rest-api-service.yaml
+kubectl apply -f crud-rest-api-deployment.yaml -f crud-rest-api-ingress.yaml -f crud-rest-api-service.yaml
 
